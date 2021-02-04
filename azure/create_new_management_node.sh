aa6f3c2a0aff81c0f15a85691411a0af5e29cf9a
Adam Łoszyn <a.loszyn@alcesit.pl>
2021-02-04 12:51:31 +0100

    cleanup of sh script

--- azure/create_new_management_node.sh
+++ azure/create_new_management_node.sh
@@ -1,7 +1,11 @@
 # the idea - generate random password for admin, generate almost identical windows/linux nodes, defining only names in vars
 az login
-az account set --subscription="bf476486-f84d-4dea-8e46-6e39dc7a1ad6" # elanco lab subscription
-# generate pass for elancoadmin
-#az keyvault secret set --vault-name "keyvault_for_mng_nodes" --name "elancoadmin" --value $PASS
+az account set --subscription=""
+az ad sp create-for-rbac --role="Contributor" -–scopes="/subscriptions/<subscription_id>"
+az login --service-principal -u <service_principal_name> -p "<service_principal_password>" --tenant "<service_principal_tenant>"
+
+
+# generate pass for admin
+#az keyvault secret set --vault-name "keyvault_for_mng_nodes" --name "admin" --value $PASS
 terraform/terraform plan
 terraform/terraform apply -var variable_name="value"
